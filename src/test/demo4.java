e83a61e0584d08623a7a7c72b26cb7ed867e5202
Phoenix <Phoenix@10.0.0.221>
2023-04-10 17:33:02 -0400

    commit

--- src/test/demo4.java
+++ src/test/demo4.java
@@ -5,7 +5,8 @@ public class demo4 {
 	public static void main(String[] args) {
 		System.out.println("demo 4...");
 	}
-	public void devide() {
-		System.out.println(6/3);
-	}
+	
 }
+//public void devide() {
+//	System.out.println(6/3);
+//}
