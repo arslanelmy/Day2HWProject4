bac6497d34bd01f0c6012c66db9a8836c8e1baf5
Phoenix <Phoenix@10.0.0.221>
2023-04-10 17:35:48 -0400

    stash commit

--- src/test/demo4.java
+++ src/test/demo4.java
@@ -5,7 +5,12 @@ public class demo4 {
 	public static void main(String[] args) {
 		System.out.println("demo 4...");
 	}
-	
+	public void devide() {
+		System.out.println(6/3);
+	}
+	public void add () {
+		System.out.println(2+3);
+	}
 }
 //public void devide() {
 //	System.out.println(6/3);
